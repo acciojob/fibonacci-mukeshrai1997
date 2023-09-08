f04a1705ea36d05f52f4fba04d04e45489570bf3
mukeshrai1997 <135120496+mukeshrai1997@users.noreply.github.com>
2023-09-08 14:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) return 0;
+  if (num === 1) return 0; // Special case as the Fibonacci series starts with 0, but some definitions start with 1.
+
+  let prev = 0;
+  let current = 1;
+
+  for (let i = 2; i <= num; i++) {
+    const next = prev + current;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
 }
 
 module.exports = fibonacci;
